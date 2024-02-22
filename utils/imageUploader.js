68f56dfeb0cbf2f9f637a318b010506f0d754191
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-02-22 09:00:43 +0530

    ALL backend Testing Done!

--- utils/imageUploader.js
+++ utils/imageUploader.js
@@ -1,7 +1,7 @@
 const Cloudinary = require('cloudinary').v2
 
 exports.uploadImageToCloudinary = async (file,folder,height,quality) =>{
-    const options ={folder};
+   try {const options ={folder};
     if(height){
         options.height=height;
     }
@@ -9,7 +9,11 @@ exports.uploadImageToCloudinary = async (file,folder,height,quality) =>{
         options.quality=quality;
     }
 
-    options.resourse_type = "auto";
-    return await Cloudinary.uploader.upload(file.tempFilePath,options);
+    options.resource_type = "auto";
+    return await Cloudinary.uploader.upload(file.tempFilePath,options);}
+    catch (error) {
+        console.error('Error uploading file to Cloudinary:', error);
+        throw error;
+      }
 }
 
